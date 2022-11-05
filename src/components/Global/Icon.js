098315afa103a9935c83f95957aca6f72c6b0136
Christian <k.christian9@web.de>
2022-11-05 18:39:32 +0100

    icon

--- src/components/Global/Icon.js
+++ src/components/Global/Icon.js
@@ -10,6 +10,10 @@ import Location from "../../media/icons/location.svg";
 import StarFull from "../../media/icons/star-full.svg";
 import StarHalf from "../../media/icons/star-half.svg";
 import StarEmpty from "../../media/icons/star-empty.svg";
+import Facebook from "../../media/icons/facebook.svg";
+import Instagram from "../../media/icons/instagram.svg";
+import Whatsapp from "../../media/icons/whatsapp.svg";
+
 
 export default function Icon({ name, height, color, ...props }) {
   let icon;
@@ -24,6 +28,10 @@ export default function Icon({ name, height, color, ...props }) {
   if (name === "starFull") icon = StarFull;
   if (name === "starHalf") icon = StarHalf;
   if (name === "starEmpty") icon = StarEmpty;
+  if (name === "facebook") icon = Facebook;
+  if (name === "instagram") icon = Instagram;
+  if (name === "whatsapp") icon = Whatsapp;
+
 
   return (
     <Wrapper src={icon} alt="icon" height={height} color={color} {...props} />
