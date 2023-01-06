d8e21e9a44f201e8ffeb2c6388cce2ef26ca9f65
Christian <k.christian9@web.de>
2023-01-06 23:38:44 +0100

    maps fix

--- src/components/Global/Maps.js
+++ src/components/Global/Maps.js
@@ -1,42 +1,48 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { MapContainer, TileLayer, Popup, Marker } from "react-leaflet";
-import L from 'leaflet';
+import L from "leaflet";
 import Marker_Icon from "../../media/icons/marker.svg";
 /**https://www.latlong.net/convert-address-to-lat-long.html */
 
 const Maps = () => {
-  const address = [50.143028, 8.15988];
-  return (
-    <MapContainer
-      center={address}
-      zoom={16}
-      scrollWheelZoom={true}
-      style={{ height: "100%", borderRadius:"10px" }}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={address} icon={iconPerson}>
-        <Popup>
-          Taunusstein
-        </Popup>
-      </Marker>
-    </MapContainer>
-  );
+  useEffect(() => {
+    const size = 64;
+    const iconPerson = new L.Icon({
+      iconUrl: Marker_Icon,
+      iconRetinaUrl: Marker_Icon,
+      iconAnchor: new L.Point(0.5 * size, 1 * size),
+      popupAnchor: new L.Point(0, -1 * size),
+      shadowUrl: null,
+      shadowSize: null,
+      shadowAnchor: null,
+      iconSize: new L.Point(1 * size, 1 * size),
+      className: "marker-icon",
+    });
+    const address = [50.143028, 8.15988];
+    var container = L.DomUtil.get("map");
+
+    if (container != null) {
+      container._leaflet_id = null;
+    }
+    var map = L.map("map").setView(address, 12);
+    L.tileLayer(
+      "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+      {
+        attribution:
+          'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: "mapbox/streets-v11",
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken:
+          "pk.eyJ1IjoidGFyLWhlbCIsImEiOiJjbDJnYWRieGMwMTlrM2luenIzMzZwbGJ2In0.RQRMAJqClc4qoNwROT8Umg",
+      }
+    ).addTo(map);
+    L.Marker.prototype.options.icon = iconPerson;
+    var marker = L.marker(address).addTo(map);
+    marker.bindPopup("Taunusstein").openPopup();
+  }, []);
+  return <div id="map" style={{ height: "100%", borderRadius: "10px", zIndex:"2" }} />;
 };
 
 export default Maps;
-
-const size = 64;
-const iconPerson = new L.Icon({
-  iconUrl: Marker_Icon,
-  iconRetinaUrl: Marker_Icon,
-  iconAnchor: new L.Point(0.5 * size, 1 * size),
-  popupAnchor: new L.Point(0, -1 * size),
-  shadowUrl: null,
-  shadowSize: null,
-  shadowAnchor: null,
-  iconSize: new L.Point(1 * size, 1 * size),
-  className:"marker-icon"
-});
--- static/fiddle.js
+++ static/fiddle.js
@@ -7,3 +7,23 @@ function isEmpty(obj) {
   
     return JSON.stringify(obj) === JSON.stringify({});
   }
+
+  const MapsWorking = () => {
+    const address = [50.143028, 8.15988];
+    return (
+      <MapContainer
+        center={address}
+        zoom={16}
+        scrollWheelZoom={true}
+        style={{ height: "100%", borderRadius: "10px" }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {/* <Marker position={address} icon={iconPerson}>
+          <Popup>Taunusstein</Popup>
+        </Marker> */}
+      </MapContainer>
+    );
+  };
